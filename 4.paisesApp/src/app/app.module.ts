989219ce65bf6e3bb9dca8df68ca393d5d62d618
Santiago JM <santiagojm84@gmail.com>
2022-08-23 11:03:51 -0500

    Avances proyecto paises

--- 4.paisesApp/src/app/app.module.ts
+++ 4.paisesApp/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PaisModule } from './pais/pais.module';
 import { SharedModule } from './shared/shared.module';
@@ -11,8 +11,9 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     SharedModule,
-    PaisModule
+    PaisModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
