c2e2cac51ef7f1f96f04e16b9f9241243c676446
Santiago JM <santiagojm84@gmail.com>
2022-09-05 11:41:45 -0500

    Avances desarrollo

--- 5.pipesApp/src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ 5.pipesApp/src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -7,13 +7,41 @@ import { interval } from 'rxjs';
   styles: [
   ]
 })
-export class NoComunesComponent  {
+export class NoComunesComponent  
+{
   nombre:string = 'Carlos';
   genero:string = 'masculino';
+  cantidad: number = 1;
 
   invitacionMapa = 
   {
     'masculino':'invitarlo',
     'femenino':'invitarla'
+  }
+
+  persona = 
+  {
+    Nombre: 'Camilo',
+    Edad: 32,
+    Genero: 'M'
+  }
+
+  //i18nPlural
+  clientes: string[] = ['Maria','Pedro','Juan','Mariana'];
+  clientesMapa = {'=0':'No tenemos ningun cliente esperando',
+                  '=1':`tenemos ${this.clientes.length} cliente esperando`,
+                  'other': `tenemos ${this.clientes.length} clientes esperando`}
+
+
+  CambiarPersona()
+  {
+    this.nombre = 'Mariana';
+    this.genero = 'femenino';
+  }
+
+  BorrarCliente()
+  {
+    this.clientes.pop();
+    console.log(this.clientes);
   }
 }
